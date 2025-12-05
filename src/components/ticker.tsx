79d46d750554e23e5d7a00cbcad7786e2b1b2e6d
bpuglieseal24dw <bpuglieseal24dw@ikzubirimanteo.com>
2025-12-05 10:50:49 +0100

    feat: agregando animaciones ticker al hero

--- src/components/ticker.tsx
+++ src/components/ticker.tsx
@@ -1,21 +1,25 @@
+import Marquee from "react-fast-marquee";
+
 export default function Ticker() {
   return (
     <ul className="flex flex-row items-center justify-center gap-10 mb-10 opacity-70">
-      <li>
-        <img src="cosmos.png" alt="Cosmos" />
-      </li>
-      <li>
-        <img src="etherum.png" alt="Ethereum" />
-      </li>
-      <li>
-        <img src="solana.png" alt="Solana" />
-      </li>
-      <li>
-        <img src="stacks.png" alt="Stacks" />
-      </li>
-      <li>
-        <img src="polygon.png" alt="Polygon" />
-      </li>
+      <Marquee>
+        <li>
+          <img src="cosmos.png" alt="Cosmos" />
+        </li>
+        <li>
+          <img src="etherum.png" alt="Ethereum" />
+        </li>
+        <li>
+          <img src="solana.png" alt="Solana" />
+        </li>
+        <li>
+          <img src="stacks.png" alt="Stacks" />
+        </li>
+        <li>
+          <img src="polygon.png" alt="Polygon" />
+        </li>
+      </Marquee>
     </ul>
   );
 }
